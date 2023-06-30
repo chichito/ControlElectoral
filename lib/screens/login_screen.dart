5d4e4f67b0fc5e0c85a2835679ac51b859e9981a
Fernando Cando Chacon <fernandocando@hotmail.com>
2023-06-30 08:06:15 -0500

    ss

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,10 +8,147 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _user = '';
+  String _userError = '';
+  bool _userShowError = false;
+
+  String _password = '';
+  String _passwordError = '';
+  bool _passwordShowError = false;
+  bool _passwordShow = false;
+
+  bool _showLoader = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Hola Menu'),
+    return Scaffold(
+      //backgroundColor: Color(0xFFFFEB3B),
+      body: Stack(
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 40,
+                ),
+                _showLogo(),
+                const SizedBox(
+                  height: 20,
+                ),
+                _showUser(),
+                _showPassword(),
+                _showButtons(),
+              ],
+            ),
+          ),
+          _showLoader
+              ? LoaderComponent(text: 'Por favor espere...')
+              : Container(),
+        ],
+      ),
     );
   }
+
+  Widget _showLogo() {
+    return Container(
+      margin: const EdgeInsets.all(0),
+      width: 100,
+      height: 100,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+            image: AssetImage('assets/control.jpg'), fit: BoxFit.cover),
+      ),
+    );
+  }
+
+  Widget _showUser() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: 'Ingresa La User...',
+          labelText: 'User',
+          errorText: _userShowError ? _userError : null,
+          prefixIcon: const Icon(Icons.verified_user_outlined),
+          suffixIcon: const Icon(Icons.supervised_user_circle),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        onChanged: (value) {
+          _user = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showPassword() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        obscureText: !_passwordShow,
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: 'Ingresa tu contraseña...',
+          labelText: 'Contraseña',
+          errorText: _passwordShowError ? _passwordError : null,
+          prefixIcon: const Icon(Icons.lock),
+          suffixIcon: IconButton(
+            icon: _passwordShow
+                ? const Icon(Icons.visibility)
+                : const Icon(Icons.visibility_off),
+            onPressed: () {
+              setState(() {
+                _passwordShow = !_passwordShow;
+              });
+            },
+          ),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        onChanged: (value) {
+          _password = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showButtons() {
+    return Container(
+      margin: const EdgeInsets.only(left: 10, right: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              _showLoginButton(),
+              const SizedBox(
+                width: 20,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _showLoginButton() {
+    return Expanded(
+      child: ElevatedButton(
+        child: const Text('Iniciar Sesión'),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith<Color>(
+              (Set<MaterialState> states) {
+            return const Color(0xFF120E43);
+          }),
+        ),
+        onPressed: () => _login(),
+      ),
+    );
+  }
+
+  void _login() {}
 }
